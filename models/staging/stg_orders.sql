b932be69fc07bcfa611fc76327de2447960ceaa9
anastasiia.volianiuk@tages.ru <anastasiia.volianiuk@tages.ru>
2025-04-14 16:04:11 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,20 @@
+SELECT 
+--from raw_orders
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice-o.ordercostprice as orderprofit,
+    o.ordersellingprice,
+    o.ordercostprice,
+--from raw_customer
+    c.customername,
+    c.segment,
+    c.country,
+--from raw_product
+    p.category,
+    p.productname,
+    p.subcategory
+FROM {{ ref('raw_orders') }} o
+LEFT JOIN {{ ref('raw_customer') }} c ON o.customerid=c.customerid
+LEFT JOIN {{ ref('raw_product') }} p ON o.productid=p.productid
